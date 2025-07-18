18cc87e2a68fa24a2afcf1eb096d9c9d78a9260b
Michael <onisoris@yandex.ru>
2025-07-18 22:23:45 +0300

    new loops added

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,5 +1,23 @@
 fn main() {
-    loop {
-        println!("again!");
+    let mut counter = 0;
+
+    let _output = loop {
+        counter += 1;
+
+        if counter > 10 {
+            break counter * 2;
+        }
+    };
+    println!("_output = {_output} \n counter = {counter}");
+
+    'pupupu: loop {
+        println!("push me");
+        loop {
+            counter -= 1;
+            println!("attach me, counter = {counter}");
+            if counter == 0 {
+                break 'pupupu;
+            }
+        }
     }
 }
