4bdcefe81acb5a6f54ce1deff66d9d1e1b921661
Michael <onisoris@yandex.ru>
2025-07-18 13:31:17 +0300

    Loops and cond added

--- control_construct/src/main.rs
+++ control_construct/src/main.rs
@@ -1,3 +1,4 @@
+use std::io;
 fn main() {
     let number = 3;
     if number < 5 {
@@ -5,4 +6,30 @@ fn main() {
     } else {
         println!("condition was false");
     }
+    println!("Write your age: ");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("somthing wrong");
+    let number_for_condition: usize = input.trim().parse().expect("Your ages very strange");
+    if number_for_condition < 18 {
+        println!("You won't to drink beer");
+    } else {
+        println!("You'll to drink beer");
+    }
+    if number == 3 {
+        println!("pupupupupupupu")
+    }
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    let cond = true;
+    let ifcond = if cond { 5 } else { 6 };
+    println!("ifcond = {ifcond}");
 }
