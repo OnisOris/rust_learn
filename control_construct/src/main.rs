314db557424c0e75e1cbd1e518c38b865b4cd849
Michael <onisoris@yandex.ru>
2025-08-02 12:56:32 +0300

    Add: some information about cycles and exit from them

--- control_construct/src/main.rs
+++ control_construct/src/main.rs
@@ -32,4 +32,44 @@ fn main() {
     let cond = true;
     let ifcond = if cond { 5 } else { 6 };
     println!("ifcond = {ifcond}");
+
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("popopopopopopopopopopopopopopo");
+
+    let a = [10, 20, 30, 40, 50];
+    for element in a {
+        println!("the value is: {element}");
+    }
+
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("OIIA OIIA OIIA OIIA");
 }
